85a236c78c37ed70ea87b3635e33530d9a51ce2e
ksushapopova <ks.popova.2005@mail.ru>
2024-11-12 22:56:43 +0300

    testExecute, testIsOperator, testIsDigit

--- src/test/java/org/example/ExecuteTest.java
+++ src/test/java/org/example/ExecuteTest.java
@@ -17,4 +17,42 @@ public class ExecuteTest {
         assertEquals("1 2 + 3 *", executor.infix_to_postfix("(1 + 2) * 3"));
         assertEquals("1 2 + 3 4 + *", executor.infix_to_postfix("(1 + 2) * (3 + 4)"));
     }
+
+    @Test
+    public void testExecute() {
+        Execute executor = new Execute();
+
+        assertEquals(7.0, executor.execute("3 4 +"), 0.001);
+        assertEquals(14.0, executor.execute("3 4 + 2 *"), 0.001);
+        assertEquals(11.0, executor.execute("3 4 2 * +"), 0.001);
+        assertEquals(4.0, executor.execute("3 4 2 * + 7 -"), 0.001);
+        assertEquals(-16796.0, executor.execute("3 4 2 * + 7 5 ^ -"), 0.001);
+        assertEquals(-8392.5, executor.execute("3 4 2 * + 7 5 ^ 2 / -"), 0.001);
+        assertEquals(9.0, executor.execute("1 2 + 3 *"), 0.001);
+        assertEquals(21.0, executor.execute("1 2 + 3 4 + *"), 0.001);
+    }
+
+    @Test
+    public void testIsOperator() {
+        Execute executor = new Execute();
+
+        assertTrue(executor.is_operator('+'));
+        assertTrue(executor.is_operator('-'));
+        assertTrue(executor.is_operator('*'));
+        assertTrue(executor.is_operator('/'));
+        assertTrue(executor.is_operator('^'));
+        assertFalse(executor.is_operator('a'));
+        assertFalse(executor.is_operator(' '));
+    }
+
+    @Test
+    public void testIsDigit() {
+        Execute executor = new Execute();
+
+        assertTrue(executor.is_digit('0'));
+        assertTrue(executor.is_digit('5'));
+        assertTrue(executor.is_digit('9'));
+        assertFalse(executor.is_digit('a'));
+        assertFalse(executor.is_digit(' '));
+    }
 }
